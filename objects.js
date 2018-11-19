a7ac5d9833fb1ecb3d8a57319e78cc6d9fb9f40d
Chris Dalla Santa <chris.dallasanta@gmail.com>
2018-11-19 06:43:20 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,3 +3,10 @@ var playlist = {
   'Whitney Houston':'I Will Always Love You'
 };
 
+function updatePlaylist(playlist,artist,song){
+  return Object.assign(playlist, {[artist]:song})
+}
+
+function removeFromPlaylist(artist){
+  
+}
