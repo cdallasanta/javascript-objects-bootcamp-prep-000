f8b0d0be7fc8cd232850f77103fb8b37d67db37d
Chris Dalla Santa <chris.dallasanta@gmail.com>
2018-11-19 06:44:42 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -7,6 +7,7 @@ function updatePlaylist(playlist,artist,song){
   return Object.assign(playlist, {[artist]:song})
 }
 
-function removeFromPlaylist(artist){
-  
+function removeFromPlaylist(playlist, artist){
+  delete playlist.artist;
+  return playlist;
 }
